ff99248c46c3ef709f70122fee9301ce7650f951
abakasam <abakasam@outlook.com>
2022-08-19 17:31:40 -0400

    hello

---
+++ programbase.js
@@ -0,0 +1,11 @@
+var programbase = {
+	engine: require('mongodb'),
+	configuration: '',
+	connection: '',
+	variable: '',
+	
+	open: '',
+	store: '',
+	reuse: '',
+	stop: ''
+}
--- server.js
+++ server.js
@@ -1,67 +1,13 @@
-var web = require('http')
-var url = require('url')
-var database = require('./database.js')
-var email = require('emailjs')
-
 var server = {
-	using: 'Heroku',
-	URL: {
-		Codeanywhere: 'https://drifty-abakasam720639.codeanyapp.com/',
-		Heroku: 'https://drifty-coachshake.herokuapp.com/'
-	},
-	redirect: {
-		LOGIN: 'council.html',
-		SCHEDULE: 'council.html',
-		REGISTER: 'council.html',
-		ORDERS: 'schedule.html',
-		OFFER: 'offer.html',
-		MENU: 'menu.html',
-		PICKUP: 'delivery.html',
-		ORDER: 'schedule.html'
-	}
+	engine: require('https'),
+	inuse: '',
+	location: '',
+	
+	upload: '',
+	direct: '',
+	slug: '',
+	database: '',
+	task: function(taskname) {},
+	filed: function(type = ['js', 'xml', 'json']) {},
+	listen: function(port) {}
 }
-
-server.web = web.createServer(function (request, response) {	
-	console.log('Request started from coachshake')
-	server.URL.parameters = url.parse(request.url, true).query
-	server.task = server.URL.parameters.task
-	console.log(server.URL.parameters)
-	
-	database.open()
-	database.call(server.task, server.URL.parameters)
-	
-	setTimeout(function () {
-		console.log('Executed query and acquired database results ', database.results)
-
-		server.redirect.location = server.URL[server.using] + server.redirect[server.task]		
-		server.URL.query = '?task=' + server.task + '&phone=' + server.URL.parameters.phone  + '&data=' + JSON.stringify(database.results)
-		
-		// IMPROVE: module.exports server.URL.parameters from server.js
-		if(server.task == 'PICKUP') {
-			server.URL.query = '?task=' + server.task + '&phone=' + server.URL.parameters.phone + '&type=' + server.URL.parameters.type + '&choice=' + server.URL.parameters.choice
-		}
-		
-		if(server.task == 'ORDER') {
-			console.log('Send email')
-
-			var mail = email.server.connect({
-				user:     "abakasam@zohomail.com", 
-				password: "WS;SsPz.", 
-				host:     "smtp.zoho.com", 
-				ssl:      true
-			})
-			
-			console.log('New Order! Phone: ' + server.URL.parameters.phone + ' Item: ' + server.URL.parameters.item + ' Location: ' + server.URL.parameters.location + ' Time: ' + server.URL.parameters.time)
-		}
-
-		response.writeHead(302, { location: server.redirect.location + server.URL.query })
-		console.log('Loaded webpage', server.redirect.location, server.URL.query)
-
-		response.end()
-		database.stop()
-	}, 2000)
-})
-
-server.web.listen(process.env.PORT || 3001, function () {
-	console.log('Server is running')
-})
